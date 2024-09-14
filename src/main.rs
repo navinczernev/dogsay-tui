41a5e1eb094ff888a304f5bfeb29879d1543e276
navinczernev <navinczernev@gmail.com>
2024-09-14 12:39:38 +0530

    Add layered dog text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use cursive::views::TextView;
+
 fn main() {
     let mut siv = cursive::default();
+    let dog_text = "Woof!
+\\
+ \\
+  /^ ^\\
+ / 0 0 \\
+ V\\ Y /V
+  / - \\
+  |    \\
+  || (__V";
 
+    siv.add_layer(TextView::new(dog_text));
     siv.run();
 }
